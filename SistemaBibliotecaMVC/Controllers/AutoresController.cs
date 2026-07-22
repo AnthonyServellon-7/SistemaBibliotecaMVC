5e593658b4e37821bf4e61226274168a2939b45e
Anthony S. <anthonyservellon4@gmail.com>
2026-07-21 21:20:52 -0600

    Agregar archivos de proyecto.

---
+++ SistemaBibliotecaMVC/Controllers/AutoresController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaBibliotecaMVC.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SistemaBibliotecaMVC.Controllers
+{
+    public class AutoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Guardo los 5 autores en una lista para mandarlos a la vista
+            var listaAutores = new List<Autor>
+            {
+                new Autor { Id = 1, Nombre = "Gabriel", Apellido = "García Márquez", Nacionalidad = "Colombiana", FechaNacimiento = new DateTime(1927, 3, 6), Activo = false },
+                new Autor { Id = 2, Nombre = "Isabel", Apellido = "Allende", Nacionalidad = "Chilena", FechaNacimiento = new DateTime(1942, 8, 2), Activo = true },
+                new Autor { Id = 3, Nombre = "Mario", Apellido = "Vargas Llosa", Nacionalidad = "Peruana", FechaNacimiento = new DateTime(1936, 3, 28), Activo = true },
+                new Autor { Id = 4, Nombre = "Julio", Apellido = "Cortázar", Nacionalidad = "Argentina", FechaNacimiento = new DateTime(1914, 8, 26), Activo = false },
+                new Autor { Id = 5, Nombre = "Laura", Apellido = "Esquivel", Nacionalidad = "Mexicana", FechaNacimiento = new DateTime(1950, 9, 30), Activo = true }
+            };
+
+            // Retorno la vista pasando la lista como modelo
+            return View(listaAutores);
+        }
+    }
+}
---
+++ SistemaBibliotecaMVC/Models/Autor.cs
@@ -0,0 +1,13 @@
+﻿namespace SistemaBibliotecaMVC.Models
+{
+    public class Autor
+    {
+        // Creo las propiedades que pidió el ingeniero para el autor
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Nacionalidad { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public bool Activo { get; set; }
+    }
+}
