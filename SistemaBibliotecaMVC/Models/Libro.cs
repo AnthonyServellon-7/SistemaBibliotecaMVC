563de7f61ec0777834d91a46bef2f599907ced3a
Anthony S. <anthonyservellon4@gmail.com>
2026-07-29 20:16:40 -0600

    feat: Módulo Libros, imágenes estáticas y actualización de Autores
    - Se agregó Editar y Eliminar al controlador de Autores.
    
    - Se creó el modelo, controlador y vistas CRUD completas para Libros.
    
    - Se resolvió el reto de imágenes con tarjetas de Bootstrap usando wwwroot/images.

---
+++ SistemaBibliotecaMVC/Models/Libro.cs
@@ -0,0 +1,13 @@
+﻿namespace SistemaBibliotecaMVC.Models
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AnioPublicacion { get; set; }
+
+        
+        public string ImagenUrl { get; set; }
+    }
+}
